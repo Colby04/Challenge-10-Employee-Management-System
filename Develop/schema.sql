456bf86b5918ec11ff38adb23d91d5b5d0f9c69d
Colby04 <cjcodes2024@gmail.com>
2024-09-22 14:35:55 -0500

    Deleted error

--- Develop/schema.sql
+++ Develop/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE role (
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INTEGER NOT NULL,
-  FOREIGN KEY (department_id) REFERENCES department(id) //Not sure if this is correct
+  FOREIGN KEY (department_id) REFERENCES department(id) 
 );
 
 CREATE TABLE employee (
